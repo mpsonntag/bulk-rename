d90173f2f02f10a90728d2f0bbfc6932b38acf5f
M. Sonntag <michael.p.sonntag@gmail.com>
2016-01-07 14:29:47 +0100

    initial commit adding readme, license and gitignore

---
+++ App.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2016, Michael Sonntag (sonntag@bio.lmu.de)
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted under the terms of the BSD License. See
+# LICENSE file in the root of the project.
+
+__author__ = 'Sonntag'
+
+class Main:
+    def run(self):
+        print("hurradiegams")
+
+Main().run()
---
+++ README.md
@@ -0,0 +1,3 @@
+# bulk-rename
+Tiny pet project trying out the IDEA python plugin with python 3.4 
+for the use of bulk renaming files from the command line. 
