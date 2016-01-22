34055df9e0cd88b09fd2ed4035c215f64be6833b
Michael Sonntag <michael.p.sonntag@gmail.com>
2016-01-22 16:41:55 +0100

    handle trailing folder separator

--- App.py
+++ App.py
@@ -32,7 +32,7 @@ class Main:
         # Define the path of the main directory from which to rename files in folders.
         main_dir_path = 'D:\\_Chaos\\Bilder\\201510_Zentralasien\\testRename\\'
         # Define a base string that will be inserted into the renaming string.
-        main_name = 'Zentralasien_'
+        main_name = 'Zentralasien'
         # Use the name of the directory when renaming a file.
         add_directory_name = True
         # Separator string that separates the part of the existing filename that will be
@@ -59,7 +59,7 @@ class Main:
                 # TODO add better check for exclude_dirs at this point
                 do_exclude = False
                 for curr_dir in exclude_dirs:
-                    if path.__contains__(curr_dir):
+                    if path.find(curr_dir) > -1:
                         print("[Info] Directory " + curr_dir + " will be excluded")
                         do_exclude = True
 
@@ -70,7 +70,7 @@ class Main:
                         original_file = os.path.join(path, f)
 
                         if rename_all_file_types or rename_file_types.__contains__(str(imghdr.what(original_file)).upper()):
-                            if not f.__contains__(name_separator):
+                            if not f.find(name_separator) > -1:
                                 # TODO integrate filename in a nicer way into the print string
                                 print("[Warning] File \'" + f + "\' does not contain separator")
                             else:
@@ -78,10 +78,15 @@ class Main:
 
                                 file_name = ""
 
+                                # Add directory name to te current file name if set.
+                                # Make sure only one white space separator is used at the
+                                # end of the directory name.
                                 if add_directory_name:
-                                    file_name = os.path.basename(path)
+                                    file_name = os.path.basename(path).rstrip(white_space_separator)
+                                    file_name += white_space_separator
 
-                                file_name = file_name + main_name + "{:03d}".format(i)
+                                file_name += main_name + white_space_separator
+                                file_name += "{:03d}".format(i)
                                 file_name += name_separator + split_parts[1]
 
                                 file_name.strip()
@@ -91,6 +96,7 @@ class Main:
                                     file_name = file_name.replace(' ', white_space_separator)
 
                                 formatted_name = os.path.join(path, file_name)
+
                                 #print("[Info] Rename to " + formatted_name)
                                 os.rename(original_file, formatted_name)
                                 i += 1
