2f3b5c435af7bcbddc765edb3090f520d2eae63f
M. Sonntag <michael.p.sonntag@gmail.com>
2018-01-04 17:47:13 +0100

    add travis before_install script

---
+++ resources/before_install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then brew update; fi
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    if [[ "$OSXENV" == "2.7" ]]; then
+        brew install python;
+        virtualenv venv -p python;
+        source venv/bin/activate;
+    else
+        brew install python3;
+        virtualenv venv -p python3;
+        source venv/bin/activate;
+    fi
+fi
+
+which python
