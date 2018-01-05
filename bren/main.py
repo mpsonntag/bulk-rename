3b1323409d29fa683d59b9b9ceb80b6cda78d260
M. Sonntag <michael.p.sonntag@gmail.com>
2018-01-05 11:30:28 +0100

    add main file

---
+++ bren/main.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2017, Michael Sonntag (sonntag@bio.lmu.de)
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted under the terms of the BSD License. See
+# LICENSE file in the root of the project.
+
+from .App import Main
+
+
+def run():
+    Main.run()
+
+
+if __name__ == '__main__':
+    print("main")
