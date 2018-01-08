9029c60b8ceb98c800578c3d7038f4fc2f94852a
M. Sonntag <michael.p.sonntag@gmail.com>
2018-01-08 15:23:09 +0100

    [resources] Fix osx conda hickup

--- resources/install_conda.sh
+++ resources/install_conda.sh
@@ -28,13 +28,22 @@ conda update -q conda
 conda info -a
 which python
 
-# Create conda environment
-conda create -q -n condaenv python=$CONDAPY
-source activate condaenv
-which python
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    conda create -q -n condaenv python=$CONDAPY
+    source activate condaenv
+    which python
 
-# Install supported gi dependencies as provided by the installation guide.
-conda install -c pkgw/label/superseded gtk3
-conda install -c conda-forge pygobject
-conda install -c conda-forge gdk-pixbuf
-conda install -c pkgw-forge adwaita-icon-theme
+    # Install supported gi dependencies as provided by the installation guide.
+    # Currently there is still a dependency mixup for conda under osx.
+    conda install -c pkgw/label/superseded gtk3
+    conda install -c conda-forge pygobject
+    conda install -c conda-forge gdk-pixbuf
+    conda install -c pkgw-forge adwaita-icon-theme
+else
+    # Add custom channels
+    conda config --add channels conda-forge
+    conda config --add channels pkgw-forge
+    # The used dependencies are not the one advertised via the readme
+    # but can be more easily installed and suffice for the tests for now.
+    conda create -q -n condaenv python=$CONDAPY gtk3 pygobject gdk-pixbuf adwaita-icon-theme
+fi
