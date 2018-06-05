4f42bf42c6dcb44f7a0972bb9c00818d087c808f
M. Sonntag <michael.p.sonntag@gmail.com>
2018-06-05 18:42:48 +0200

    [setup] Add file

---
+++ setup.py
@@ -0,0 +1,26 @@
+try:
+    from setuptools import setup
+except ImportError as ex:
+    from distutils.core import setup
+
+packages = [
+    'bren'
+]
+
+with open('README.rst') as f:
+    description_text = f.read()
+
+install_req = ["pyyaml"]
+
+setup(
+    name='bulkrename',
+    version='1.0.0',
+    description='bulk file rename',
+    author='Michael Sonntag',
+    packages=packages,
+    test_suite='test',
+    install_requires=install_req,
+    include_package_data=True,
+    long_description=description_text,
+    license="BSD"
+)
