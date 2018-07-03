119f60ffe6d29fe32f47516b1cd0a5300f5d1418
M. Sonntag <michael.p.sonntag@gmail.com>
2018-07-03 16:29:24 +0200

    [osx virtualenv] add pyenv 2.7

--- resources/install_osx_virtualenv.sh
+++ resources/install_osx_virtualenv.sh
@@ -5,17 +5,19 @@ echo Running install_osx_virtalenv.sh
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
 
     brew update;
+    brew install pyenv;
+    brew upgrade pyenv;
+    brew install pyenv-virtualenv;
+    eval "$(pyenv init -)";
+    eval "$(pyenv virtualenv-init -)";
 
     if [[ "$OSXENV" == "2.7" ]]; then
-        brew install python;
-        virtualenv venv -p python;
-        source venv/bin/activate;
+        pyenv install 2.7.14
+        pyenv virtualenv 2.7.14 venv
+#        brew install python;
+#        virtualenv venv -p python;
+#        source venv/bin/activate;
     else
-        brew install pyenv;
-        brew upgrade pyenv;
-        brew install pyenv-virtualenv;
-        eval "$(pyenv init -)";
-        eval "$(pyenv virtualenv-init -)";
         if [[ "$OSXENV" == "3.5" ]]; then
             pyenv install 3.5.0
             pyenv virtualenv 3.5.0 venv
@@ -29,4 +31,5 @@ fi
 
 which python;
 python --version;
+pip install requests;
 pip install lxml;
